f93022ae9723f20b65167b84bffde588bb5b997a
xiekun <857812506@qq.com>
2019-08-07 10:13:42 +0800

    init

--- fdfs/src/main/java/com/kechun/GetwayApplication.java
+++ fdfs/src/main/java/com/kechun/GetwayApplication.java
@@ -1,22 +1,24 @@
 package com.kechun;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-/**
- * 中心服务
- */
-@EnableDiscoveryClient
-@EnableEurekaServer
+@EnableScheduling
+@EnableEurekaClient
 @SpringBootApplication
-public class CenterServerApplication  {
+public class GetwayApplication {
+
 
     public static void main(String[] args) {
-        SpringApplication.run(CenterServerApplication.class, args);
+        SpringApplication.run(GetwayApplication.class, args);
     }
+
 }
 
