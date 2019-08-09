38b99ab96acbee22cfa3005516f425d58463fae1
xiekun <857812506@qq.com>
2019-08-09 17:04:42 +0800

    init DataSourceConfiguration

--- common/src/main/java/com/kechun/conf/DataSourceConfiguration.java
+++ common/src/main/java/com/kechun/conf/DataSourceConfiguration.java
@@ -4,7 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
